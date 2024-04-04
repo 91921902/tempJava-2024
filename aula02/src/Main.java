2b1557991e5e9edceb5e2ffa35162636f828fcfb
ZUMA ELMA PEREIRA <zuma_pereira@estudante.sesisenai.org.br>
2024-04-04 17:24:22 -0300

    arquivos da aula 2

---
+++ aula02/src/Main.java
@@ -0,0 +1,139 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Nome: ");
+//        String nome = teclado.nextLine();
+//        System.out.print("Nota: ");
+//        float nota = teclado.nextFloat();
+//
+//        System.out.printf("A nota do aluno %s foi %.1f", nome, nota);
+
+        // criar um projeto pra ler o nome e 3 notas do aluno;
+        // calcule a média dele
+        // duvido dizer se ele passou ou não
+
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Nome: ");
+//        String nome = teclado.nextLine();
+//        System.out.print("Nota 1: ");
+//        float nota1 = teclado.nextFloat();
+//        System.out.print("Nota 2: ");
+//        float nota2 = teclado.nextFloat();
+//        System.out.print("Nota 3: ");
+//        float nota3 = teclado.nextFloat();
+//
+//        System.out.printf("A notas do aluno %s foram %.1f %.1f %.1f\n", nome, nota1, nota2, nota3);
+//
+//        float media = ( nota1 + nota2 + nota3)/3;
+//
+//        System.out.printf("A média do aluno %s foi %.1f\n", nome, media);
+//
+//        if(media >=7){
+//            System.out.println("Passou");
+//        }else{
+//            System.out.println("Não foi desta vez");
+//        }
+
+
+
+//        JOptionPane.showMessageDialog(null,"Olá mundo");
+//         int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade:"));
+//        System.out.println(idade);
+
+//        System.out.println( JOptionPane.showConfirmDialog(null,"Tem certeza?"));
+//        System.out.println( JOptionPane.showConfirmDialog(null,"Tem certeza?"));
+//        System.out.println( JOptionPane.showConfirmDialog(null,"Tem certeza?"));
+
+//        Escreva um programa que determine se um número é positivo, negativo ou zero.
+//        let num = prompt("Digite um número:");
+//        if (num > 0) {
+//            console.log("O número é positivo.");
+//        } else if (num < 0) {
+//            console.log("O número é negativo.");
+//        } else {
+//            console.log("O número é zero.");
+//        }
+//
+//        Scanner digite = new Scanner(System.in);
+//        System.out.print("Digite um numero inteiro: ");
+//        int numero = digite.nextInt();
+//        if(numero > 0){
+//           System.out.println("O numero é positivo");
+//       }else {
+//           System.out.println("O numero é negativo");
+//       }
+
+
+//        Crie um programa que verifique se um ano é bissexto.
+//        let ano = prompt("Digite um ano:");
+//        if ((ano % 4 === 0 && ano % 100 !== 0) || ano % 400 === 0) {
+//            console.log("O ano é bissexto.");
+//        } else {
+//            console.log("O ano não é bissexto.");
+//        }
+//       Scanner digite = new Scanner(System.in);
+//       System.out.print("Digite um ano: ");
+//       int ano = digite.nextInt();
+//        if((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0){
+//          System.out.printf("O ano %d é bissexto\n", ano);
+//      }else {
+//           System.out.printf("O ano %d não é bissexto\n", ano);
+//      }
+
+
+//        Desenvolva um programa que determine se um triângulo é escaleno, isósceles ou equilátero, com base nos comprimentos dos seus lados.
+//        let lado1 = parseInt(prompt("Digite o comprimento do primeiro lado:"));
+//        let lado2 = parseInt(prompt("Digite o comprimento do segundo lado:"));
+//        let lado3 = parseInt(prompt("Digite o comprimento do terceiro lado:"));
+//
+//        if (lado1 === lado2 && lado2 === lado3) {
+//            console.log("O triângulo é equilátero.");
+//        } else if (lado1 !== lado2 && lado2 !== lado3 && lado1 !== lado3) {
+//            console.log("O triângulo é escaleno.");
+//        } else {
+//            console.log("O triângulo é isósceles.");
+//        }
+//
+//        Scanner triangulo = new Scanner(System.in);
+//        System.out.print("Digite o comprimento do primeiro lado: ");
+//        float lado1 = triangulo.nextFloat();
+//        System.out.print("Digite o comprimento do segundo lado lado: ");
+//        float lado2 = triangulo.nextFloat();
+//        System.out.print("Digite o comprimento do terceiro lado lado lado: ");
+//        float lado3 = triangulo.nextFloat();
+//
+//        if (lado1 == lado2 && lado2 == lado3){
+//         System.out.println("O triângulo é equilátero.");
+//        }else if(lado1 != lado2 && lado2 != lado3 && lado1 != lado3) {
+//         System.out.println("O triangulo é escaleno");
+//    }else{
+//            System.out.println("O triângulo é isósceles.");
+//    }
+
+//        Escreva um programa que determine se um número é par e maior que 10.
+//        let num = prompt("Digite um número:");
+//        if (num % 2 === 0 && num > 10) {
+//            console.log("O número é par e maior que 10.");
+//        } else {
+//            console.log("O número não é par ou não é maior que 10.");
+//        }
+
+//        Scanner numeroPar = new Scanner(System.in);
+//        System.out.print("Digite um numero inteiro: ");
+//        int numero = numeroPar.nextInt();
+//        if (numero % 2 == 0 && numero > 10) {
+//            System.out.printf("O número %d é par e maior que 10.", numero);
+//        }else {
+//            System.out.printf("O número %d não é par ou não é maior que 10.", numero);
+//        }
+
+
+
+    }
+}
