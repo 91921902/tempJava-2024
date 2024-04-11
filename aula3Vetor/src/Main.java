e51050154f8ece5cd04ff755b40c3e80789b08a5
Zuma Elma Pereira <132463705+91921902@users.noreply.github.com>
2024-04-11 17:23:01 -0300

    aula3vetor

--- aula3Vetor/src/Main.java
+++ aula3Vetor/src/Main.java
@@ -1,7 +1,28 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
-    
+//    **Ler n salários e:**
+//
+//    **(a) encontrar o maior**
+//
+//    **(b) aplicar 10% de aumento a quem ganha menos de R$2500,00**
+//
+//    **(c) mostrar o total da folha de pagamento antiga**
+//
+//    **(d) mostrar o total da folha corrigida**
+//
+//    Você pode escolher entre `perguntar ao usuário quantos salários serão digitados` ou `ler salários até que seja digitado um salário de zero reais`.
+//
+
+        int numeroSalarios;
+        numeroSalarios = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidades de salários "));
+        System.out.println(numeroSalarios);
+
+
+
+
+
+    }
 }
