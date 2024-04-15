9b2ddc62a98b5ba3522bf54efa1156ab07b36d24
Zuma Elma Pereira <zumapereira@hotmail.com>
2024-04-15 10:37:27 -0300

    pronto

--- aula3Vetor/src/Main.java
+++ aula3Vetor/src/Main.java
@@ -15,14 +15,52 @@ public class Main {
 //
 //    Você pode escolher entre `perguntar ao usuário quantos salários serão digitados` ou `ler salários até que seja digitado um salário de zero reais`.
 //
+        Scanner scanner = new Scanner(System.in);
+        float[] salarios = new float[100];
+        int numSalarios = 0;
+        float maiorSalario = 0;
+        float folhaAntiga = 0;
+        float folhaCorrigida = 0;
 
-        int numeroSalarios;
-        numeroSalarios = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidades de salários "));
-        System.out.println(numeroSalarios);
+        while (true) {
+            System.out.print("Digite o próximo salário (ou 0 para parar): R$ ");
+            float salario = scanner.nextFloat();
 
+            if (salario == 0) {
+                break;
+            }
 
+            salarios[numSalarios] = salario;
+            numSalarios++;
 
+            if (salario > maiorSalario) {
+                maiorSalario = salario;
+            }
 
+            if (salario < 2500) {
+                salario *= 1.1;
+                folhaCorrigida += salario;
+                System.out.println("Salário com aumento de 10%: R$" + salario);
+            } else {
+                folhaCorrigida += salario;
+                System.out.println("Salário: R$" + salario);
+            }
 
+            folhaAntiga += salario;
+        }
+
+        if (maiorSalario == 0) {
+            System.out.println("Nenhum salário foi digitado.");
+        } else {
+            System.out.println("O maior salário digitado é: R$" + maiorSalario);
+        }
+
+        System.out.println("Total da folha de pagamento antiga: R$" + folhaAntiga);
+        System.out.println("Total da folha de pagamento corrigida: R$" + folhaCorrigida);
+
+        scanner.close();
     }
 }
+
+
+
